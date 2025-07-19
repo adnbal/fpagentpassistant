635bfccedf227f22da26622f7970fb5660d982ce
adnbal <99461328+adnbal@users.noreply.github.com>
2025-07-19 13:43:54 +1200

    Update voice_api.py

--- voice_api.py
+++ voice_api.py
@@ -1,3 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+from botpress_client import BotpressClient
+
+app = FastAPI()
+
+API_ID = os.getenv("BOTPRESS_CHAT_API_ID") or "bot_16e43556-ccfc-4fea-b39b-a9eefac04ef3"
+USER_KEY = os.getenv("BOTPRESS_TOKEN") or "bp_pat_hkH6xKDFL4ddPurtB4qgVYumQKmFv8MHeEFB"
+
+class VoiceInput(BaseModel):
+    text: str
+
 @app.post("/api/voice")
 async def handle_voice_input(data: VoiceInput):
     user_text = data.text
